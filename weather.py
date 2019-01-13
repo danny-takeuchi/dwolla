21a44a352ce0bf4be7cd5f07bb261298c018ce17
Danny Takeuchi <dannytakeuchi@DN0a1f4cc1.SUNet>
2019-01-13 13:25:26 -0800

    Updated weather.py

--- weather.py
+++ weather.py
@@ -1,16 +1,22 @@
 import sys
 import requests
 import json
+import math
 
 
 loc = None
 if len(sys.argv) == 1:
-    loc = raw_input("Where are you? ")
+    loc = input("Where are you? ")
 elif len(sys.argv) == 2:
     loc = sys.argv[1]
 else:
     print("Please run script with the command \'python weather.py\' or \'python weather.py <location>\'")
 
-response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + loc + "&APPID=0124b9ecc3f7e62e522eca1c30afbc30")
-temp = response.json()["main"]["temp"]
-print("Chicago weather:\n" + temp + " degrees Fahreneheit")
+response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=" + loc + "&APPID=0124b9ecc3f7e62e522eca1c30afbc30&units=imperial")
+obj = response.json()
+code = obj["cod"]
+if code == "404":
+    print("City not found")
+else:
+    tempF = round(obj["main"]["temp"])
+    print(loc + " weather:\n" + str(tempF) + " degrees Fahreneheit")
